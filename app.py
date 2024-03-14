00013ef5d22455a3f3cffa0e4c35fe4f61b16c6c
bensullivan2002 <bensullivan2002@gmail.com>
2024-03-14 10:50:36 +0000

    Setup Route 53 record to target bigben.codes to S3 bucket.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ PortfolioWebsiteStack(app, "PortfolioWebsiteStack",
                       # Uncomment the next line if you know exactly what Account and Region you
                       # want to deploy the stack to. */
 
-                      # env=cdk.Environment(account='123456789012', region='us-east-1'),
+                      env=cdk.Environment(account='177242513726', region='eu-west-2'),
 
                       # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                       )
--- portfolio_website/infrastructure/portfolio_website_stack.py
+++ portfolio_website/infrastructure/portfolio_website_stack.py
@@ -5,6 +5,8 @@ from aws_cdk import (
     aws_s3 as s3,
     RemovalPolicy,
     aws_s3_deployment as s3deploy,
+    aws_route53 as route53,
+    aws_route53_targets as targets,
 )
 from constructs import Construct
 
@@ -23,6 +25,7 @@ class PortfolioWebsiteStack(Stack):
             block_public_access=s3.BlockPublicAccess(block_public_policy=False),
             public_read_access=True,
             website_index_document="index.html",
+            bucket_name="bigben.codes"
         )
 
         # Deploy the bucket as a static website
@@ -31,3 +34,11 @@ class PortfolioWebsiteStack(Stack):
             sources=[s3deploy.Source.asset('./portfolio_website/web-content')],
             destination_bucket=bucket,
         )
+
+        # Provision a DNS record for the website
+        record = route53.RecordSet(self, "AliasRecord",
+                                   record_name="bigben.codes",
+                                   record_type=route53.RecordType.A,
+                                   target=route53.RecordTarget.from_alias(targets.BucketWebsiteTarget(bucket)),
+                                   zone=route53.HostedZone.from_lookup(self, "HostedZone", domain_name="bigben.codes")
+                                   )
